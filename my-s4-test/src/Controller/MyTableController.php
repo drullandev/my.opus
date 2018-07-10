d8e6a16da5bd7f322f2d54d383680b372e120ac1
David Rullán Díaz <drullan.github@gmail.com>
2018-07-10 16:48:20 +0200

    Index perfectly running

--- my-s4-test/src/Controller/MyTableController.php
+++ my-s4-test/src/Controller/MyTableController.php
@@ -3,36 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\MyTable;
-//use App\Repository\MyTableRepository;
-use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MyTableController extends Controller
 {
-    protected $entityManager;
 
-    public function index(EntityManagerInterface $entityManager)
+    public function index()
     {
-        $repository = $entityManager->getDoctrine()->getRepository(MyTable::class)->findAll();
-        /*
-        // you can fetch the EntityManager via $this->getDoctrine()
-        // or you can add an argument to your action: index(EntityManagerInterface $entityManager)
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $myTable = new MyTable();
-        $myTable->setName('Keyboard');
-
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($myTable);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
-        return new Response('Saved new product with id '.$myTable->getId());
-        return $this->render('my_table/index.html.twig', [
-            'controller_name' => 'MyTableController',
-        ]);*/
+        echo 'Hello!';
+        //$repository = $this->getDoctrine()->getRepository(MyTable::class)->findAll();
     }
+
 }
